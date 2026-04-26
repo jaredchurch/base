f1236770b62c42edca1f7d0b413947b2c30e06fa
Jared <81801445+jaredchurch@users.noreply.github.com>
2026-04-26 10:12:10 +0000

    missed item

--- .devcontainer/features/spec-kit/install.sh
+++ .devcontainer/features/spec-kit/install.sh
@@ -6,6 +6,7 @@ set -e
 # export DEBIAN_FRONTEND=noninteractive
 
 uv tool install specify-cli --from git+https://github.com/github/spec-kit.git
+uv tool update-shell
 echo "✅ Install Spec Kit"
 
 ### End of File
